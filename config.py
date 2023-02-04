57125f1a5fe8c0395918fd25b83cae7f01fc4cd9
The RituRajPS <theriturajps@gmail.com>
2023-02-04 12:08:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -12,8 +12,8 @@ API_HASH = os.environ.get("API_HASH", "8e959f089c05109d1c5f8ea4aec8af21")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "5670697466:AAGnqQsgMHrVLXV_CoK6XKKZFvH5zeQ0cOk")
 ADMINS = [int(i.strip()) for i in os.environ.get("ADMINS").split("1161352331")] if os.environ.get("ADMINS") else []
 ADMIN = ADMINS
-DATABASE_NAME = os.environ.get("DATABASE_NAME", "Db Name")
-DATABASE_URL = os.getenv("DATABASE_URL", "Monfo url") 
+DATABASE_NAME = os.environ.get("DATABASE_NAME", "URLShortener")
+DATABASE_URL = os.getenv("DATABASE_URL", "mongodb+srv://riturajps:riturajps@cluster0.vqauwt1.mongodb.net/?retryWrites=true&w=majority") 
 OWNER_ID =  int(os.environ.get("OWNER_ID", "1161352331")) 
 ADMINS.append(OWNER_ID) if OWNER_ID not in ADMINS else []
 ADMINS.append(Id Owned Id)
