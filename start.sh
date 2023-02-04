fc77b306b8e4f84bd3fedcd24e51cf6ef8bbc69e
The RituRajPS <theriturajps@gmail.com>
2023-02-04 12:11:45 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo...."
-git clone https://github.com/Cyniteofficial/Shortner-Converter-Bot-V2.git /Shortner-Converter-Bot-V2
-cd /Shortner-Converter-Bot-V2
+git clone https://github.com/theriturajps/Shortener-Converter-Bot.git /Shortener-Converter-Bot
+cd /Shortener-Converter-Bot
 pip3 install -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
